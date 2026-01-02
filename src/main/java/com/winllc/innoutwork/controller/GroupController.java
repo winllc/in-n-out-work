307b8a3928c52929f702ae407e46c40016f51ffc
David Winters <dave.t.winters@gmail.com>
2026-01-02 17:04:45 -0500

    UI updates

--- src/main/java/com/winllc/innoutwork/controller/GroupController.java
+++ src/main/java/com/winllc/innoutwork/controller/GroupController.java
@@ -5,6 +5,7 @@ import com.winllc.innoutwork.data.LdapDn;
 import com.winllc.innoutwork.data.reports.GroupReport;
 import com.winllc.innoutwork.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,6 +24,8 @@ public class GroupController {
     private ReportService reportService;
 
     @PostMapping("/report")
+    @PreAuthorize("hasAnyAuthority(T(com.winllc.innoutwork.constant.UserRoleEnum).ADMIN, " +
+            "T(com.winllc.innoutwork.constant.UserRoleEnum).MANAGER)")
     public ModelAndView generateReport(@ModelAttribute("form") GenerateReportForm form){
         ModelAndView mav = new ModelAndView("groupReport");
 
